cf0509e40eb9dd0103121e2b76556ed865150464
Mohan Das <mohan132414@gmail.com>
2023-04-17 22:30:30 +0530

    first change

---
+++ CoolCareAdminpanel/Controllers/AdminController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CoolCareAdminpanel.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        public ActionResult Login()
+        {
+            return View();
+        }
+        public ActionResult DashBoard()
+        {
+            return View();
+        }
+
+        public ActionResult UserDetail()
+        {
+            return View();
+        }
+
+        public ActionResult Services()
+        {
+            return View();
+        }
+
+        public ActionResult Packages()
+        {
+            return View();
+        }
+
+        public ActionResult Technician()
+        {
+            return View();
+        }
+
+        public ActionResult Feedback()
+        {
+            return View();
+        }
+    }
+}
